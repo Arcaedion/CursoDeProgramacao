868f2f55dae67f4fd9442d7a5e46351f6aeab550
Arcaedion <arcaedionlab@gmail.com>
2019-03-16 16:08:15 +0000

    Adicionado aula e gabarito

---
+++ Aulas/Aula 3 - Estruturas de Repetição/EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Imprimindo um valor 3 vezes manualmente
+            int valor = 10;
+            Console.Out.WriteLine(valor);
+            valor += 1;
+            Console.Out.WriteLine(valor);
+            valor += 1;
+            Console.Out.WriteLine(valor);
+            valor += 1;
+            Console.Out.WriteLine(valor);
+
+
+            // Exemplo: Investimento de 1000 reais, cada mês lucra-se 1% desse dinheiro investido.
+            // R$ 1000  -> R$ 1126,82
+            // 1º mes: 1000 + 1%(1000) = 1010
+            // 2º mes: 1010 + 1%(1010) = 1020,10
+            // E por aí vai. Quanto teria depois de 12 meses?
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("Ciclo for");
+
+            double valorInvestido = 1000.0;
+            Console.Out.WriteLine("valorInvestido inicial: " + valorInvestido);
+            for (int mes = 1;mes <= 12;mes+=1)
+            {
+                valorInvestido = valorInvestido * 1.01;
+            }
+            Console.Out.WriteLine("valorInvestido apos 12 meses: " + valorInvestido);
+
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("Ciclo while");
+            double valorInvestido2 = 1000.0;
+            Console.Out.WriteLine("valorInvestido2 inicial: " + valorInvestido2);
+            int mes2 = 1;
+            while (mes2 <= 12)
+            {
+                valorInvestido2 = valorInvestido2 * 1.01;
+                mes2 += 1;
+            }
+            Console.Out.WriteLine("valorInvestido2 apos 12 meses: " + valorInvestido2);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Aulas/Aula 3 - Estruturas de Repetição/Gabarito/Gabarito.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasDeRepeticao
+{
+    class Gabarito
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Exercícios");
+            Console.Out.WriteLine("1) ");
+            Console.Out.WriteLine("Letra D) porque no ciclo for, o valorInvestido está sendo multiplicado por 1, 12 vezes, portanto o valorInvestido não mudará após 12 meses.");
+            // Prova:
+            double valorInvestido = 3450.0;
+            for (int i = 1; i <= 12; i += 1)
+            {
+                valorInvestido = valorInvestido * 1.00;
+            }
+            Console.Out.WriteLine("valorInvestido Após 12 meses: " + valorInvestido);
+
+            // --------------------------------
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("2)");
+            int soma1Ate100 = 0;
+            for(int a = 1; a <= 100; a++) // Repare no ++, este operador de incremento adiciona 1 a uma varíavel!
+            {
+                soma1Ate100 += a;
+            }
+            Console.Out.WriteLine("A soma dos números de 1 até 100 é: "+ soma1Ate100);
+
+            // --------------------------------
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("3) ");
+            int val = 10;
+            while (val < 25)
+            {
+                val += 10;
+            }
+            Console.Out.WriteLine("Letra B: 30.  Após a primeira passagem pelo while, val vai ter o valor 20, e, ");
+            Console.Out.WriteLine("como 20 é menor que 25, vai passar de novo. O novo valor agora é 30 e agora não entraremos no for");
+
+            // --------------------------------
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("4) ");
+            int numero = 2;
+            int potencia = 5;
+            int resultadoPotencia = 1; ;
+            for(int b = 1; b <= 5; b++)
+            {
+                resultadoPotencia *= numero;
+            }
+            Console.Out.WriteLine("O resultado é: "+resultadoPotencia);
+
+            // --------------------------------
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("5) ");
+            // Novo código
+            int exercicioIncremento = 10;
+            exercicioIncremento++;
+            Console.Out.WriteLine("exercicioIncremento: " + exercicioIncremento);
+
+            // --------------------------------
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("6) ");
+            // Codigo com do while
+            int doWhile = 10;
+            do
+            {
+                doWhile += 10;
+            } while (doWhile < 5);
+
+            Console.Out.WriteLine("doWhile: " + doWhile);
+            Console.Out.WriteLine("Resposta: Não. No código anterior, a variável doWhile seria 10, nesse é 20. ");
+            Console.Out.WriteLine("A razão para isso acontecer é que o do...while garante que ao menos uma execução aconteça");
+            Console.Out.WriteLine("Independente da condição while.");
+
+            // --------------------------------
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("7) ");
+            for (int m = 1; m <= 100; m++) // Repare no ++, este operador de incremento adiciona 1 a uma varíavel!
+            {
+                if(m % 3 == 0)
+                    Console.Out.Write(m + ";"); // Write é similar ao WriteLine, com a diferença que ele não pula linhas
+            }
+            
+
+
+
+
+            Console.Read();
+        }
+    }
+}
